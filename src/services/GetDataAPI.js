c6bc10f42c7530b0fe89b006cdb1d24fc08bc524
martin-b-07e5 <martin.bergagno@gmail.com>
2022-08-02 07:55:41 -0300

    space removed

--- src/services/GetDataAPI.js
+++ src/services/GetDataAPI.js
@@ -3,7 +3,7 @@ const API_KEY = "d2b71d9bb99a4725b1c3ba0d40163a73";
 // const API_KEY = "88589059d5eb4758aba90c7bdaab4932";
 
 export default async function GetDataAPI(search, page) {
-  const URL = `https://newsapi.org/v2/everything?q=${search}&page=${page}&pageSize=10&language=es  &apiKey=${API_KEY}`;
+  const URL = `https://newsapi.org/v2/everything?q=${search}&page=${page}&pageSize=10&language=es&apiKey=${API_KEY}`;
   const data = await fetch(URL);
 
   return data.json();

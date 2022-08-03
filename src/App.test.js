e63784c3d51a52453ef0f903a48b64723f2f10f8
martin-b-07e5 <martin.bergagno@gmail.com>
2022-08-02 23:56:55 -0300

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,8 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<App />); // renderizamos el componente
+  const linkElement = screen.getByText(/learn react/i); // obtenemos el rtdo
+  expect(linkElement).toBeInTheDocument(); // testeamos rtdo que sea true
+  expect(linkElement).toBeInTheDocument(); // testeamos rtdo que sea true es un// testeamos rtdo que sea true
 });
--- src/components/Search.test.jsx
+++ src/components/Search.test.jsx
@@ -1,22 +1,35 @@
+// https://github.com/testing-library/react-testing-library
+import "@testing-library/jest-dom";
+// NOTE: jest-dom adds handy assertions to Jest and is recommended, but not required
+
 import { render, screen } from "@testing-library/react";
-import { useSearchParams } from "react-router-dom";
 import { Search } from "./Search";
+// import { useSearchParams } from "react-router-dom";
 
-// comportamiento
-// mostramos un buscador, y cuando buscamos algo »
-// » se modifica lo que se muestra » llamamos userParam de react-router-dom
-// el btn tiene que estar deshabilitado
+/* comportamiento
+Este test solo verifica que se renderice
+mostramos un buscador, y cuando buscamos algo » se modifica lo que se muestra
+» llamamos userParam de react-router-dom
+el btn tiene que estar deshabilitado
 
-// mockeamos la dependencia.(si no encuentra la dependencia »
-// » cada vez que llamamos a este metodo » devolver jest.fn) (jest.fn es algo cualquiera)
-// jest.mock es una función
-// devolvemos un resultado que controlamos
+mockeamos la dependencia.(si no encuentra la dependencia »
+» cada vez que llamamos a este metodo » devolver jest.fn) (jest.fn es algo cualquiera)
+jest.mock es una función
+Devolvemos un resultado que controlamos
+La dependencia externa "react-router-dom" la metemos adentro de la función
+  useSearchParams va a retornar un arreglo, que como primer parametro,
+    tiene un objeto que retorna una función get
+  y como segundo parametro retornamos una función que no me importa que hace, 
+    en este caso jest.fn()
+Esto da un 100% de cobertura de esta prueba.
+ */
+
 jest.mock("react-router-dom", () => ({
   useSearchParams: () => [
     {
-      get: () =>
-        "analizamos que vamos a devolver " +
-        "» Esto devuelve un objeto que tiene una función que retorna un string",
+      get: () => "prueba",
+      // "analizamos que vamos a devolver " +
+      // "» Esto devuelve un objeto, que tiene una función, que retorna un string",
     },
     jest.fn(),
   ],
@@ -28,17 +41,3 @@ describe("<Search />", () => {
     expect(buscador).toBeInTheDocument();
   });
 });
-
-// describe("<Empty />", () => {
-//   it("Debe renderizarse", async () => {
-//     render(<Empty />); // renderizamos el componente
-//     const resultado = screen.getByText("No Results"); // obtenemos el rtdo
-//     expect(resultado).toBeInTheDocument(); // testeamos rtdo que sea true
-//   });
-// });
-
-// test("Search 'No Results' by me", () => {
-//   render(<Empty />); // renderizamos el componente
-//   const result = screen.getByText("No Results"); // obtenemos el rtdo
-//   expect(result).toBeInTheDocument(); // testeamos rtdo que sea true
-// });

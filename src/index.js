fbd5365b63fc8be4583acdc0172d2931bd1e92b6
martin-b-07e5 <martin.bergagno@gmail.com>
2022-08-11 21:59:54 -0300

    added a comment

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  // https://mariosfakiolas.com/blog/my-react-components-render-twice-and-drive-me-crazy/
   // <React.StrictMode>
   <App />
   // </React.StrictMode>
